ec3a661c5f2d67a6fcb249ecd8e73c9ddf74c8e1
Aviroop-001 <banerjeeaviroop01@gmail.com>
2022-06-26 19:29:53 +0530

    Added Chat creation and accessing all chats for a user routes

--- controllers/chatControllers.js
+++ controllers/chatControllers.js
@@ -2,29 +2,36 @@ const Chat = require("../models/Chat");
 const User = require("../models/User");
 
 const createChat = async (req,res) =>{
-    const otherUserid = req.body;
-    const chatExists = await Chat.find(
-        { $and:[ 
-            { isGroup: false },
-            { users : { $elemMatch: { _id: req.user._id } }},
-            { users : { $elemMatch: { _id: otherUserid } }}
-        ]}
-    ).populate("users", "-password").populate("latestMessage");
+    const otherUser = req.body;
+    let chatExists = await Chat.find({
+            isGroup: false ,
+            $and: [
+                { users : { $elemMatch: { $eq: req.user._id } }},
+                { users : { $elemMatch: { $eq: otherUser.id } }}
+            ]
+    }).populate("users", "-password").populate("latestMessage");
+    console.log("first----", chatExists);
 
-    // chatExists= await User.populate
+    // chatExists= await User.populate(chatExists, {
+    //     path: "latestMessage.sender",
+    //     select: "username email profilepic"
+    // })
+    // console.log("second----", chatExists);
+
     if(chatExists.length >0){
-        res.status(200).send(chatExists[0]);
+        res.status(202).send(chatExists[0]);
     }
     else{
         try {
             let newChat = new Chat({
                 chatName: "ABC",
                 isGroup: false,
-                users: [req.user._id , otherUserid],
+                users: [req.user._id , otherUser.id],
             });
             const newlySavedChat = await newChat.save();
-            const populatedSavedChat = newlySavedChat.populate("users","-password");
-            res.status(200).send(populatedSavedChat);
+            const populatedSavedChat = await newlySavedChat.populate("users","-password");
+            res.status(201).send(populatedSavedChat);
+            console.log("third----", populatedSavedChat);
         } catch (err) {
             res.status(500).send(err);
         }
@@ -32,12 +39,19 @@ const createChat = async (req,res) =>{
 }
 
 const fetchUserChats = async (req,res) =>{
-
+    try {
+        let userChats = await Chat.find(
+            { users : { $elemMatch: { $eq: req.user._id } }}
+        ).populate("users", "-password")
+        .populate("latestMessage")
+        .populate("admin", "-password")
+        .sort({updatedAt: -1})
+        res.status(202).send(userChats);
+    } catch (err) {
+        res.status(500).send(err);
+        console.log(err);
+    }
 }
 
-const createGroupChat = async (req,res) =>{
 
-}
-
-
-module.exports = { createChat, fetchUserChats, createGroupChat }; 
+module.exports = { createChat, fetchUserChats}; 
--- routes/chats.js
+++ routes/chats.js
@@ -3,15 +3,10 @@ const User = require("../models/User");
 const Chat = require("../models/Chat");
 const { authorize } = require("../middleware/authMiddleware");
 
-const { createChat, fetchUserChats, createGroupChat } = require("../controllers/chatControllers");
+const { createChat, fetchUserChats } = require("../controllers/chatControllers");
 
 //The router.route() function returns an instance of a single route that you can then use to handle HTTP verbs with optional middleware.
-
 router.route('/').post(authorize, createChat);
-// router.route('/').get(authorize, fetchUserChats);
-// router.route('/create-group').post(authorize, createGroupChat);
-// router.route('/rename-group').put(renameGroupCHat);
-// router.route('/add-user-to-group').put(renameGroupCHat);
-
+router.route('/').get(authorize, fetchUserChats);
 
 module.exports = router 
